10eabfff9764b023bd6801d97f8e577dbc1cbbae
Simon Chabrier <simonchabrier@gmail.com>
2024-01-17 07:28:53 +0100

    update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const isAuthenticated = require('../security/isAuthenticated');
 
 const userRoutes = require('./userRoutes');
 const ressourceRoutes = require('./ressourceRoutes');
 const authRoutes = require('./authRoutes');
 const contactRoutes = require('./contactRoutes');
 
+// sécurité
+const isAuthenticated = require('../security/isAuthenticated');
 const verifyToken = require('../security/jwtCheck');
-
 
 // routes fermées (authentification requise)
 // router.use('/users',isAuthenticated, userRoutes);
-// router.use('/ressources',isAuthenticated, ressourceRoutes); // routes fermées (authentification requise)
-// router.use('/users', verifyToken, userRoutes); // routes fermées (token jwt requis)
+// router.use('/ressources',isAuthenticated, ressourceRoutes); 
+
+// routes fermées (JWT requis)
+// router.use('/users', verifyToken, userRoutes);
 
 // routes ouvertes préfixées par /api
 router.use('/', authRoutes);
-router.use('/users', verifyToken, userRoutes);
+router.use('/users', userRoutes);
 router.use('/ressources', ressourceRoutes);
 router.use('/contact', contactRoutes);
 
