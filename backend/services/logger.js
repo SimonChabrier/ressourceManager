68df5070b34cf0b4e61e01e398aff308f91d2b98
Simon Chabrier <simonchabrier@gmail.com>
2024-01-27 11:52:58 +0100

    add logs

--- backend/services/logger.js
+++ backend/services/logger.js
@@ -15,9 +15,13 @@ const logger = {
             console.log(`******************* Duree -----> ${duration}ms`);
             if (req.body) {
                 console.log(`******************* Body ------> ${JSON.stringify(req.body)}`);
+                // on logge le success dans un fichier
+                logger.logSucessToFile(req, res, next);
             }
             if (res.statusCode >= 400) {
                 console.error(`******************* Erreur -----> ${res.statusMessage}`);
+                // on utilise logErrorsToFile pour logger les erreurs dans un fichier
+                logger.logErrorsToFile(req, res, next);
             }
             if (res.locals.message) {
                 console.log(`******************* Message -----> ${res.locals.message}`);
@@ -29,6 +33,62 @@ const logger = {
         console.error(err.stack);
         res.status(500).send('Erreur interne du serveur retournée dans app.js');
     },
+
+    logSucessToFile: (req, res, next) => {
+
+        const fs = require('fs');
+        const path = require('path');
+        const logFile = path.join(__dirname, '../logs/success_log.json');
+
+        const method = req.method;
+        const url = req.url;
+        const code = res.statusCode;
+        const body = req.body;
+        const ip = req.ip;
+        const origin = req.get('Origin');
+        const date = new Date();
+        // créer un objet qui contient toutes les infos de la requête
+        const success = { method, url, code, body, ip, origin, date };
+        // convertir l'objet en JSON
+        const successJSON = JSON.stringify(success);
+        // écrire le JSON dans le fichier de log
+        fs.appendFile(logFile, `${successJSON}\n`, (err) => {
+            if (err) { 
+                console.error('Erreur lors de l\'écriture dans le fichier de succès:', err);
+            }
+        });
+        next();
+    },
+    
+    logErrorsToFile: (req, res, next) => {
+        
+        const fs = require('fs');
+        const path = require('path');
+        const logFile = path.join(__dirname, '../logs/errors_log.json');
+
+
+        const message = res.locals.message;
+        const code = res.statusCode;
+        const body = req.body;
+        const url = req.url;
+        const method = req.method;
+        const ip = req.ip;
+        const origin = req.get('Origin');
+        const date = new Date();
+        // on crée un objet qui contient toutes les infos de la requête
+        const error = { message, code, body, url, method, ip, origin, date };
+        // on convertit l'objet en JSON
+        const errorJSON = JSON.stringify(error);
+        // on écrit le JSON dans le fichier de log
+        fs.appendFile(logFile, `${errorJSON}\n`, (err) => {
+            if (err) { 
+                console.error('Erreur lors de l\'écriture dans le fichier d\'erreur:', err);
+            }
+        });
+        next(err);
+    },
+
+
 }
 
 module.exports = logger;
