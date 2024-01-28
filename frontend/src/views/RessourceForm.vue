208bdec7ede4dd4478ce9437a2d184ef2a07b514
Simon Chabrier <simonchabrier@gmail.com>
2024-01-28 13:59:31 +0100

    update post ressource

--- frontend/src/views/RessourceForm.vue
+++ frontend/src/views/RessourceForm.vue
@@ -1,23 +1,46 @@
 <template>
     <div>
       <!-- Formulaire de création de ressource -->
-      <form>
-        <input type="text" v-model="title" placeholder="Titre de la ressource" />
-        <input type="text" v-model="content" placeholder="Description" />
-        <!-- select de tags : doc, tuto, article... -->
-        <select v-model="tag">
-          <option value="doc">Documentation</option>
-          <option value="tuto">Tutoriel</option>
-          <option value="article">Article</option>
-        </select>
-        <!-- select de tech : eg : react, node, express, sequelize, postgresql -->
-        <select v-model="tech">
-          <option value="react">React</option>
-          <option value="node">Node</option>
-          <option value="express">Express</option>
-          <option value="sequelize">Sequelize</option>
-          <option value="postgresql">Postgresql</option>
-        </select>
+      <form class="resource-form">
+        <div class="form-group">
+          <label for="title">Titre de la ressource</label>
+          <input type="text" v-model="title" id="title" placeholder="Saisissez le titre" />
+        </div>
+  
+        <div class="form-group">
+          <label for="textArea">Description</label>
+          <textarea v-model="content" id="textArea" placeholder="Saisissez la description"></textarea>
+        </div>
+  
+        <div class="form-group">
+          <label for="tag">Tag</label>
+          <select v-model="tag" id="tag">
+            <!-- valeur par défaut placeholder -->
+            <option value="" disabled selected hidden>Choisir un tag</option>
+            <option value="doc">Documentation</option>
+            <option value="tuto">Tutoriel</option>
+            <option value="article">Article</option>
+          </select>
+        </div>
+  
+        <div class="form-group">
+          <label for="tech">Technologie</label>
+          <select v-model="tech" id="tech">
+            <!-- valeur par défaut placeholder -->
+            <option value="" disabled selected hidden>Choisir une technologie</option>
+            <option value="html">HTML</option>
+            <option value="node">Node</option>
+            <option value="express">Express</option>
+            <option value="sequelize">Sequelize</option>
+            <option value="vue">Vue</option>
+            <option value="sql">SQL</option>
+            <option value="javascript">Javascript</option>
+            <option value="css">CSS</option>
+            <option value="git">Git</option>
+            <option value="ubuntu">Ubuntu</option>
+          </select>
+        </div>
+  
         <button @click="createRessource">Créer</button>
       </form>
     </div>
@@ -32,13 +55,13 @@
   
     setup() {
       const ressourcesStore = useRessourcesStore();
-      if(ressourcesStore.connectedUser === null) {
+      if (ressourcesStore.connectedUser === null) {
         router.push({ name: 'login' });
-      } else{
+      } else {
         const userId = ressourcesStore.connectedUser.id;
         console.log(userId);
-        return { ressourcesStore, userId} 
-    }
+        return { ressourcesStore, userId };
+      }
     },
   
     data() {
@@ -67,6 +90,49 @@
   };
   </script>
   
-  <style lang = "scss" scoped>
+  <style lang="scss" scoped>
+  .resource-form {
+    max-width: 90%;
+    margin: auto;
+  }
+  
+  .form-group {
+    margin-bottom: 15px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+  
+  input,
+  textarea,
+  select {
+    width: 100%;
+    padding: 10px;
+    margin-top: 5px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
+  textarea {
+    height: 400px;
+  }
+  
+  button {
+    background-color: #4caf50;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  
+  button:hover {
+    background-color: #45a049;
+  }
   </style>
   
